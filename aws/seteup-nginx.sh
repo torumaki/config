e3434d068d1ecd910701941e3429f5bda7e3a7f5
torumaki <tmaki@furelo.jp>
2015-03-27 12:21:24 +0900

    Update seteup-nginx.sh

--- aws/seteup-nginx.sh
+++ aws/seteup-nginx.sh
@@ -1,1 +1,9 @@
+#
+yum -y install nginx
+chkconfig nginx on
 
+mkdir /var/log/httpd
+cd /etc/nginx
+curl https://raw.githubusercontent.com/torumaki/config/master/nginx/nginx.conf -o nginx.conf
+
+service nginx start
